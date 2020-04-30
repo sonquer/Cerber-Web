c497ff4f212eeb17b73d492ff8e33fd9c70b52af
sonquer <sonquer@o2.pl>
2020-04-30 20:12:00 +0100

    Added monaco editor and modal window

--- src/views/availability/Availability.tsx
+++ src/views/availability/Availability.tsx
@@ -15,10 +15,28 @@ import {
     TabPanels,
     TabList,
     TabPanel, 
-    Tab 
+    Tab,
+    Modal,
+    ModalBody,
+    ModalOverlay,
+    ModalContent,
+    ModalFooter,
+    ModalHeader,
+    ModalCloseButton,
+    Button
 } from '@chakra-ui/core';
+import Editor from "@monaco-editor/react";
 
-class Availability extends Component<{match: any}> {
+class Availability extends Component<{match: any}, {isOpen: boolean, value: string}> {
+    constructor(props: {match: any}) {
+        super(props);
+
+        this.state = {
+            isOpen: false,
+            value: ''
+        };
+    }
+
     render() {
         const { match } = this.props;
         const { params } = match;
@@ -52,19 +70,19 @@ class Availability extends Component<{match: any}> {
                     <TabPanels>
                         <TabPanel>
                             <List spacing={3} marginTop={5}>
-                                <ListItem>
+                                <ListItem onClick={() => this.onOpen('{}')}>
                                     <ListIcon icon="check-circle" color="green.500" />
                                     <small>[2020.04.01 16:00]</small> <Code>200 OK</Code> in <Code>31ms</Code>
                                 </ListItem>
-                                <ListItem>
+                                <ListItem onClick={() => this.onOpen('{}')}>
                                     <ListIcon icon="check-circle" color="green.500" />
                                     <small>[2020.04.01 16:01]</small> <Code>200 OK</Code> in <Code>11ms</Code>
                                 </ListItem>
-                                <ListItem>
+                                <ListItem onClick={() => this.onOpen('{}')}>
                                     <ListIcon icon="check-circle" color="green.500" />
                                     <small>[2020.04.01 16:02]</small> <Code>200 OK</Code> in <Code>43ms</Code>
                                 </ListItem>
-                                <ListItem>
+                                <ListItem onClick={() => this.onOpen('{}')}>
                                     <ListIcon icon="warning" color="pink.500" />
                                     <small>[2020.04.01 16:03]</small> <Code>500 Internal server error</Code> in <Code>7010ms</Code>
                                 </ListItem>
@@ -75,8 +93,35 @@ class Availability extends Component<{match: any}> {
                         </TabPanel>
                     </TabPanels>
                 </Tabs>
+                <Modal isOpen={this.state.isOpen} onClose={this.onClose} size='full'>
+                    <ModalOverlay />
+                    <ModalContent>
+                    <ModalHeader>
+                        <Code>500 Internal server error</Code>
+                    </ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <Editor
+                            height="50vh"
+                            language="javascript"
+                            value={this.state.value} />
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button variantColor="teal" mr={3} onClick={this.onClose}>Close</Button>
+                    </ModalFooter>
+                    </ModalContent>
+                </Modal>
             </div>
         );
+    }
+
+    onOpen = (value: string) => {
+        this.setState({isOpen: true, value: value});
+    }
+
+    onClose = () => {
+        this.setState({isOpen: false, value: ''});
     }
 }
 
